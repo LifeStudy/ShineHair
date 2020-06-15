be8c3188cc21b7e6cecc8734b5e990bab94c53d6
ArjDev69 <bruno96araujo@gmail.com>
2020-06-15 15:06:30 -0300

    envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '87ae8910f896cb',
+        pass: 'df916bce1c788f',
+    },
+    default: {
+        from: 'Equipe ShineHair <noreply@shinehair.com>',
+    },
+};
