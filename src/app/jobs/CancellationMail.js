24fd15cf42c71cfffcebe3e12cdcd7b69e610ecf
ArjDev69 <bruno96araujo@gmail.com>
2020-06-16 16:16:05 -0300

    monitorando falhas nas filas

--- src/app/jobs/CancellationMail.js
+++ src/app/jobs/CancellationMail.js
@@ -9,7 +9,6 @@ class CancellationMail {
 
     async handle({ data }) {
         const { appointment } = data;
-        console.log('gdghpk');
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento Cancelado',
